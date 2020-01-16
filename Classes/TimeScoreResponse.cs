1532e6dabfbb61b7ab98b51c6d3d69da7cc82aeb
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2020-01-16 15:47:17 +0000

    JSON Loop Workaround

---
+++ Classes/TimeScoreResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APITrivialMaze.Classes
+{
+    public class TimeScoreResponse
+    {
+        public int ID { get; set; }
+        public double Time { get; set; }
+        public string PlayerUsername { get; set; }
+    }
+}
--- Controllers/TimeScoresController.cs
+++ Controllers/TimeScoresController.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
+using APITrivialMaze.Classes;
 using APITrivialMaze.Data;
 using APITrivialMaze.Models;
 
@@ -17,59 +18,59 @@ namespace APITrivialMaze.Controllers
     {
         private APITrivialMazeContext db = new APITrivialMazeContext();
 
-        // GET: api/TimeScores
-        public IQueryable<TimeScore> GetTimeScores()
-        {
-            return db.TimeScores;
-        }
+        //// GET: api/TimeScores
+        //public IQueryable<TimeScore> GetTimeScores()
+        //{
+        //    return db.TimeScores;
+        //}
 
-        // GET: api/TimeScores/5
-        [ResponseType(typeof(TimeScore))]
-        public IHttpActionResult GetTimeScore(int id)
-        {
-            TimeScore timeScore = db.TimeScores.Find(id);
-            if (timeScore == null)
-            {
-                return NotFound();
-            }
+        //// GET: api/TimeScores/5
+        //[ResponseType(typeof(TimeScore))]
+        //public IHttpActionResult GetTimeScore(int id)
+        //{
+        //    TimeScore timeScore = db.TimeScores.Find(id);
+        //    if (timeScore == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return Ok(timeScore);
-        }
+        //    return Ok(timeScore);
+        //}
 
-        // PUT: api/TimeScores/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutTimeScore(int id, TimeScore timeScore)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// PUT: api/TimeScores/5
+        //[ResponseType(typeof(void))]
+        //public IHttpActionResult PutTimeScore(int id, TimeScore timeScore)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            if (id != timeScore.ID)
-            {
-                return BadRequest();
-            }
+        //    if (id != timeScore.ID)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            db.Entry(timeScore).State = EntityState.Modified;
+        //    db.Entry(timeScore).State = EntityState.Modified;
 
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TimeScoreExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!TimeScoreExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return StatusCode(HttpStatusCode.NoContent);
-        }
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
 
         // POST: api/TimeScores
         [ResponseType(typeof(TimeScore))]
@@ -86,21 +87,58 @@ namespace APITrivialMaze.Controllers
             return CreatedAtRoute("DefaultApi", new { id = timeScore.ID }, timeScore);
         }
 
-        // DELETE: api/TimeScores/5
-        [ResponseType(typeof(TimeScore))]
-        public IHttpActionResult DeleteTimeScore(int id)
+        // GET
+        [ResponseType(typeof(List<TimeScoreResponse>))]
+        public IHttpActionResult GetTimeScore(int amount)
         {
-            TimeScore timeScore = db.TimeScores.Find(id);
-            if (timeScore == null)
+            List<TimeScore> timeScores = db.TimeScores.OrderBy(n => n.Time).Take(amount).ToList();
+            List<TimeScoreResponse> response = new List<TimeScoreResponse>();
+            foreach (var item in timeScores)
+            {
+                response.Add(new TimeScoreResponse() { ID = item.ID, PlayerUsername = item.PlayerUsername, Time = item.Time });
+            }
+
+            if (response == null)
             {
                 return NotFound();
             }
 
-            db.TimeScores.Remove(timeScore);
-            db.SaveChanges();
-
-            return Ok(timeScore);
+            return Ok(response);
         }
+
+        [ResponseType(typeof(List<TimeScoreResponse>))]
+        public IHttpActionResult GetTimeScore(string username)
+        {
+            List<TimeScore> timeScores = db.TimeScores.OrderBy(n => n.Time).Where(n => n.PlayerUsername == username).Take(10).ToList();
+            List<TimeScoreResponse> response = new List<TimeScoreResponse>();
+            foreach (var item in timeScores)
+            {
+                response.Add(new TimeScoreResponse() { ID = item.ID, PlayerUsername = item.PlayerUsername, Time = item.Time });
+            }
+
+            if (response == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(response);
+        }
+
+        //// DELETE: api/TimeScores/5
+        //[ResponseType(typeof(TimeScore))]
+        //public IHttpActionResult DeleteTimeScore(int id)
+        //{
+        //    TimeScore timeScore = db.TimeScores.Find(id);
+        //    if (timeScore == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    db.TimeScores.Remove(timeScore);
+        //    db.SaveChanges();
+
+        //    return Ok(timeScore);
+        //}
 
         protected override void Dispose(bool disposing)
         {
@@ -117,3 +155,4 @@ namespace APITrivialMaze.Controllers
         }
     }
 }
+
